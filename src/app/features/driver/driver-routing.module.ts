9d314a1701de246185671c6f8fae9840d8196a2d
saulocapistrano <saulocapistrano@gmail.com>
2026-02-11 10:50:44 -0300

    feat: adiciona rotas de em rota e histórico na área do entregador

--- src/app/features/driver/driver-routing.module.ts
+++ src/app/features/driver/driver-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DriverHomeComponent } from './pages/driver-home/driver-home.component';
+import { DriverHistoryComponent } from './pages/driver-history/driver-history.component';
+import { DriverInRouteComponent } from './pages/driver-in-route/driver-in-route.component';
 
 const routes: Routes = [
   { path: '', component: DriverHomeComponent },
+  { path: 'in-route', component: DriverInRouteComponent },
+  { path: 'history', component: DriverHistoryComponent },
 ];
 
 @NgModule({
