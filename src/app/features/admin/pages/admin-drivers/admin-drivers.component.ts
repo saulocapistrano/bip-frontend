4936755b7734bf48dda3ae9725a5ad415318054f
saulocapistrano <saulocapistrano@gmail.com>
2026-02-11 10:47:54 -0300

    feat: adiciona página de entregadores na área admin

---
+++ src/app/features/admin/pages/admin-drivers/admin-drivers.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-drivers',
+  standalone: false,
+  templateUrl: './admin-drivers.component.html',
+  styleUrls: ['./admin-drivers.component.scss'],
+})
+export class AdminDriversComponent {}
