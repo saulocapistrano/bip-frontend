1f94a0f7c8ee2805db5ec1481a49bcc18943e10d
saulocapistrano <saulocapistrano@gmail.com>
2026-02-11 10:45:37 -0300

    feat: registra páginas placeholder do admin no módulo admin

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared.module';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminHomeComponent } from './pages/admin-home/admin-home.component';
+import { AdminPendingUsersComponent } from './pages/admin-pending-users/admin-pending-users.component';
+import { AdminClientsComponent } from './pages/admin-clients/admin-clients.component';
+import { AdminDriversComponent } from './pages/admin-drivers/admin-drivers.component';
+import { AdminDeliveriesComponent } from './pages/admin-deliveries/admin-deliveries.component';
+import { AdminFinancialComponent } from './pages/admin-financial/admin-financial.component';
 
 
 @NgModule({
   declarations: [
-    AdminHomeComponent
+    AdminHomeComponent,
+    AdminPendingUsersComponent,
+    AdminClientsComponent,
+    AdminDriversComponent,
+    AdminDeliveriesComponent,
+    AdminFinancialComponent
   ],
   imports: [
+    SharedModule,
     CommonModule,
     AdminRoutingModule
   ]
