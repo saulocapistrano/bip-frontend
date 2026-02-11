d8afaefa6debca80dc77aee4b8ed0ac0d047fb39
saulocapistrano <saulocapistrano@gmail.com>
2026-02-11 10:45:16 -0300

    feat: adiciona rotas do menu admin para páginas placeholder

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminHomeComponent } from './pages/admin-home/admin-home.component';
+import { AdminClientsComponent } from './pages/admin-clients/admin-clients.component';
+import { AdminDeliveriesComponent } from './pages/admin-deliveries/admin-deliveries.component';
+import { AdminDriversComponent } from './pages/admin-drivers/admin-drivers.component';
+import { AdminFinancialComponent } from './pages/admin-financial/admin-financial.component';
+import { AdminPendingUsersComponent } from './pages/admin-pending-users/admin-pending-users.component';
 
 const routes: Routes = [
   { path: '', component: AdminHomeComponent },
+  { path: 'pending-users', component: AdminPendingUsersComponent },
+  { path: 'clients', component: AdminClientsComponent },
+  { path: 'drivers', component: AdminDriversComponent },
+  { path: 'deliveries', component: AdminDeliveriesComponent },
+  { path: 'financial', component: AdminFinancialComponent },
 ];
 
 @NgModule({
