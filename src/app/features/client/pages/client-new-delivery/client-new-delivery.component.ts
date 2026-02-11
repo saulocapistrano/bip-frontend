6ade2aacb9b0c73feae9842416db213507d91c15
saulocapistrano <saulocapistrano@gmail.com>
2026-02-11 10:50:23 -0300

    feat: adiciona página de nova entrega na área cliente

---
+++ src/app/features/client/pages/client-new-delivery/client-new-delivery.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-new-delivery',
+  standalone: false,
+  templateUrl: './client-new-delivery.component.html',
+  styleUrls: ['./client-new-delivery.component.scss'],
+})
+export class ClientNewDeliveryComponent {}
